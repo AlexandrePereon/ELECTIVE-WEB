050b2f5fbc3123db23b44225683021613e5c1a6a
AlexandrePereon <alexandre.pereon.pro@gmail.com>
2024-03-31 20:29:57 +0200

    test 3

--- config/config.js
+++ config/config.js
@@ -11,6 +11,9 @@ if (env === "production" || env === "test"|| env === "local") {
   console.log("BaseURL : " + envConfig.BASE_URI);
   console.log("BaseENDPOINT : " + envConfig.BASE_ENDPOINT);
   console.log("---------------------------");
+  console.log("DB_NAME : " + env.DB_NAME);
+  console.log("DB_USER : " + env.DB_USER);
+  console.log("DB_PASS : " + env.DB_PASS);
 
   Object.keys(envConfig).forEach((key) => {
     process.env[key] = envConfig[key];
