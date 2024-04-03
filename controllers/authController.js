9338b6406f0d9c8538491ec7ecc51cb6d09ce394
Leo_44 <98652077+OnlyLeoo@users.noreply.github.com>
2024-04-03 12:29:32 +0200

    Suppression (#29)
    * supression d'un restau

--- controllers/authController.js
+++ controllers/authController.js
@@ -306,6 +306,18 @@ const authController = {
     await user.destroy();
     return res.status(200).send({ message: 'Utilisateur supprimé' });
   },
+
+  // DELETE /auth/restaurant/delete
+  deleteUserRestaurant: async (req, res) => {
+    const { id } = req.body.userData;
+    const user = await User.findByPk(id);
+    if (!user) return res.status(404).send({ error: 'Utilisateur non trouvé.' });
+    user.restaurant = null;
+
+    await user.save();
+    logger.log('info', 'Restaurant de l\'utilisateur supprimé', { userID: user.id });
+    return res.status(200).send({ message: 'Restaurant de l\'utilisateur supprimé' });
+  },
 };
 
 export default authController;
