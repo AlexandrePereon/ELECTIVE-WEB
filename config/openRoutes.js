8768c69c3ee35ffbfb5575d50ae21119ac37e833
AlexandrePereon <alexandre.pereon.pro@gmail.com>
2024-04-08 13:21:01 +0200

    openRoutes all GET

--- config/openRoutes.js
+++ config/openRoutes.js
@@ -2,12 +2,9 @@
 
 const openRoutes = [
   { path: '/', method: 'OPTIONS' },
-  { path: '/restaurant/', method: 'GET' },
-  { path: '/order/api-docs', method: 'GET' },
-  { path: '/auth/api-docs', method: 'GET' },
+  { path: '/', method: 'GET' },
   { path: '/auth/register', method: 'POST' },
   { path: '/auth/login', method: 'POST' },
-  { path: '/auth/verify', method: 'GET' },
   { path: '/auth/refresh', method: 'POST' },
 ];
 
